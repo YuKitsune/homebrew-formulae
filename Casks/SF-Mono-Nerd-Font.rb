9f08d256e905d44925cee4593454b12cda76bf6c
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2024-02-02 20:43:01 +1000

    Fix stanza ordering

--- Casks/SF-Mono-Nerd-Font.rb
+++ Casks/SF-Mono-Nerd-Font.rb
@@ -1,15 +1,15 @@
-cask 'sf-mono-nerd-font' do
+cask "sf-mono-nerd-font" do
   version "0.0.14"
   sha256 "d8de7a1e87ead7a5e13cb41e456d83e5d580c06b1e580e21c6e28927e55122be"
 
   url "https://github.com/yukitsune/Cupertino/archive/v#{version}.zip"
+  name "SF-Mono-Nerd-Font"
+  homepage "https://github.com/YuKitsune/Cupertino"
+
   livecheck do
     url :url
     strategy :github_latest
   end
-
-  name "SF-Mono-Nerd-Font"
-  homepage "https://github.com/YuKitsune/Cupertino"
 
   font "SF-Mono-Nerd-Font-v#{version}/SFMonoNerdFont-Bold.otf"
   font "SF-Mono-Nerd-Font-v#{version}/SFMonoNerdFont-BoldItalic.otf"
